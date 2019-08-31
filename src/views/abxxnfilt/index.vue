bd8c08989b66d37cdc23658025159b371bf8adb7
ligen <ligenmt@163.com>
2019-08-31 20:26:08 +0800

    update

--- src/views/abxxnfilt/index.vue
+++ src/views/abxxnfilt/index.vue
@@ -5,7 +5,10 @@
           <mt-button icon="back">back</mt-button>
         </router-link>
       </mt-header>
-      <div>
+      <div style="margin: 0 auto; padding: 10px; text-align: center">
+        <mt-button id="abxxn_filt_btn" type="primary" data-clipboard-target="#abxxn_filt">复制</mt-button>
+      </div>
+      <div id="abxxn_filt">
          <span v-for="item in filtResult" style="margin: 2px;">
             {{item}}
           </span>
@@ -15,6 +18,8 @@
 </template>
 
 <script>
+
+    import Clipboard from 'clipboard'
 
     export default {
         name: "abxxnfilt",
@@ -26,11 +31,28 @@
 
       mounted() {
         let f = localStorage.getItem('abxxnfilt')
-        console.log('filt', f)
-        this.filtResult = JSON.parse(localStorage.getItem('abxxnfilt'));
+        this.filtResult = JSON.parse(f);
         localStorage.clear();
+
+        var clipboard = new Clipboard('#abxxn_filt_btn');
+        clipboard.on('success', function(e) {
+          console.info('Action:', e.action);
+          console.info('Text:', e.text);
+          console.info('Trigger:', e.trigger);
+          e.clearSelection();
+        });
+
+        clipboard.on('error', function(e) {
+          console.error('Action:', e.action);
+          console.error('Trigger:', e.trigger);
+        });
+        // document.execCommand("Copy")
+        // alert("已复制好，可贴粘。");
       },
       methods: {
+          onCopy() {
+
+          },
       }
     }
 </script>
